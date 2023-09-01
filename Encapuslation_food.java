f06e1ed1e433d4983792f286178bad62543b6436
eshasharmagithub <98895538+eshasharmagithub@users.noreply.github.com>
2023-09-01 14:46:21 +0530

    Add files via upload

---
+++ Encapuslation_food.java
@@ -0,0 +1,22 @@
+package ApnaCollegePlacements;
+
+public class Encapuslation_food {
+    String food_name;
+    int food_price;
+
+    void setname(String food_name){
+        this.food_name=food_name;
+    }
+
+    public String getname() {
+        return food_name;
+    }
+
+    void setcost(int food_price){
+        this.food_price=food_price;
+    }
+    public int getcost(){
+        return food_price;
+
+    }
+}
---
+++ Encasulation_food_main.java
@@ -0,0 +1,19 @@
+package ApnaCollegePlacements;
+
+public class Encasulation_food_main {
+    public static void main(String[] args) {
+        //now we will make an object which you have to ratofy
+        Encapuslation_food fd=new Encapuslation_food();
+
+       //for the food name
+       fd.setname("samosa");
+
+        System.out.println(fd.getname());
+
+fd.setcost(3888);
+
+        System.out.println(fd.getcost());
+        }
+
+    }
+
